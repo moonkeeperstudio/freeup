99584033ff3fa53b0851d6bcc5969e6d358cb1d1
Mark Guiang <mahikgot@gmail.com>
2026-01-31 16:21:44 +0800

    chore: remove accidentally commited build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,21 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+const eslintConfig = [
+  // Global ignores must come first
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "node_modules/**",
+      "*.config.js",
+      "*.config.mjs",
+    ],
+  },
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+];
 
 export default eslintConfig;
--- freeup/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wWu/VZVjve0qhhwEP9sz5ZY+W6oL0chXmMiNGitZnJQ=\"\n}"
